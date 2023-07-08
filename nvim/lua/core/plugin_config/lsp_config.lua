0a1a520ca6179a778ed0736780e87910ea3fd515
Ben De Bonis <ben.debonis@gmail.com>
2023-07-08 12:33:13 -0700

    enable ghost_text

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -23,6 +23,12 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
+cmp.setup {
+    experimental = {
+        ghost_text = true,
+    }
+}
+
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
